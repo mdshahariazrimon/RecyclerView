4185834486412a1646b8b4ea8a3d5790a80cf632
mdshahariazrimon <mriaz182048@gmail.com>
2022-11-27 00:30:01 +0600

    Fianl Step

--- app/src/main/java/cseian/com/MainActivity.java
+++ app/src/main/java/cseian/com/MainActivity.java
@@ -1,16 +1,20 @@
 package cseian.com;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Button listbtn,gridbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+        listbtn=findViewById(R.id.listbtn);
+        gridbtn=findViewById(R.id.gridbtn);
 
         ArrayList<Food> foods=new ArrayList<>();
         foods.add(new Food("Salad View",R.drawable.img));
@@ -28,7 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
         FoodAdapter adapter=new FoodAdapter(this,foods);
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        //linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
+
+        listbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+        gridbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(gridLayoutManager);
+            }
+        });
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
     }
